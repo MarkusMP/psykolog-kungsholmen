b774a697f31edb410865e8ae33fc21510a5dfed6
Markus Persson <markusperssonbiz@gmail.com>
2023-02-14 23:33:30 +0100

    Install TailwindCSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center text-6xl font-bold uppercase tracking-wider">
+      Hello World
+    </div>
+  );
 }
