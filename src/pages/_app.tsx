b2fc1f06761270ccb703bad98aea75b3ea8e0043
Markus Persson <markusperssonbiz@gmail.com>
2023-02-14 23:57:50 +0100

    Use @next/font for Google Inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({
+  weight: ["400", "600", "700"],
+  subsets: ["latin"],
+  style: ["normal"],
+  variable: "--font-inter",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${inter.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center text-6xl font-bold uppercase tracking-wider">
+    <div className="min-h-screen flex items-center text-center justify-center text-6xl font-bold uppercase tracking-wider">
       Hello World
     </div>
   );
