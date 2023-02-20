a89ac2c056e18ad55508f8bef9963dcef975bc9a
Markus Persson <markusperssonbiz@gmail.com>
2023-02-20 10:10:24 +0100

    Added sv for language and added favicon

--- src/components/global/SiteMeta.tsx
+++ src/components/global/SiteMeta.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export function SiteMeta({
   description,
@@ -7,37 +7,59 @@ export function SiteMeta({
   noIndex = false,
   canonical,
 }: {
-  description?: string
-  ogImage?: string
-  title?: string
-  noIndex: boolean
-  canonical?: string | null
+  description?: string;
+  ogImage?: string;
+  title?: string;
+  noIndex: boolean;
+  canonical?: string | null;
 }) {
   return (
     <Head>
       <title>{title && title}</title>
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="shortcut icon" href="/favicon.ico" />
       <meta name="msapplication-TileColor" content="#F9FAFB" />
       <meta name="msapplication-config" content="/browserconfig.xml" />
       <meta name="theme-color" content="#F9FAFB" />
-      <meta key="description" name="description" content={description && description} />
+      <meta
+        key="description"
+        name="description"
+        content={description && description}
+      />
 
       {canonical && <link rel="canonical" href={canonical} key="canonical" />}
 
       {ogImage && <meta property="og:title" content={title && title} />}
       {ogImage && <meta property="og:image" content={ogImage} />}
-      {ogImage && <meta property="og:description" content={description && description} />}
+      {ogImage && (
+        <meta property="og:description" content={description && description} />
+      )}
       {ogImage && canonical && <meta property="og:url" content={canonical} />}
       {ogImage && <meta property="og:image:width" content="1200" />}
       {ogImage && <meta property="og:image:height" content="630" />}
 
       {noIndex && <meta key="robots" name="robots" content="noindex,follow" />}
-      {noIndex && <meta key="googlebot" name="googlebot" content="noindex,follow" />}
+      {noIndex && (
+        <meta key="googlebot" name="googlebot" content="noindex,follow" />
+      )}
     </Head>
-  )
+  );
 }
