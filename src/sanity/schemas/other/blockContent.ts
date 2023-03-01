a880e0ee934c23b17acd326846757afa7389196c
Markus Persson <markusperssonbiz@gmail.com>
2023-03-01 18:57:12 +0100

    Change card list padding and add h3 and h4 to blockcontent

--- src/sanity/schemas/other/blockContent.ts
+++ src/sanity/schemas/other/blockContent.ts
@@ -12,7 +12,11 @@ export const blockContent = defineType({
       // correspond with HTML tags, but you can set any title or value
       // you want and decide how you want to deal with it where you want to
       // use your content.
-      styles: [{ title: "Normal", value: "normal" }],
+      styles: [
+        { title: "Normal", value: "normal" },
+        { title: "H3", value: "h3" },
+        { title: "H4", value: "h4" },
+      ],
       lists: [{ title: "Bullet", value: "bullet" }],
       // Marks let you mark up inline text in the block editor.
       marks: {
